1bbd00a300252a549cdbb1c5141928f609215b27
IMRANDIL <aliimranadil2@gmail.com>
2022-02-22 08:38:02 +0530

    yh

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import styles from './App.module.css';
 
-import { Cards, Chart, CountryPicker } from './components'
+import { Cards, Chart, CountryPicker } from './components';
+
+
+import { fetchData } from './Api';
+
+
 
 class App extends React.Component {
+
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    console.log(data);
+  }
+
+
+
+
   render() {
     return (
       <div className={styles.container} >
