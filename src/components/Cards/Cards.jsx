6f1672eae065acfee9bc2bac7279a14b69eba64a
IMRANDIL <aliimranadil2@gmail.com>
2022-02-22 09:08:22 +0530

    yh

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React from 'react';
 
-const Cards = () => {
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+
+import styles from './Cards.module.css'
+
+
+
+const Cards = ({ data }) => {
+    console.log(data);
     return (
-        <div>Cards</div>
+        <div className={styles.container}>
+            <Grid container spacing={3}
+                direction="row"
+                justifyContent="center"
+                alignItems="center">
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color='textSecondary' gutterBottom>Infected</Typography>
+                        <Typography variant='h5'>REAL DATA</Typography>
+                        <Typography color='textSecondary'>REAL DATE</Typography>
+                        <Typography variant='body2'>Number of Active Cases of Covid-19</Typography>
+                    </CardContent>
+                </Grid>
+
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color='textSecondary' gutterBottom>Recovered</Typography>
+                        <Typography variant='h5'>REAL DATA</Typography>
+                        <Typography color='textSecondary'>REAL DATE</Typography>
+                        <Typography variant='body2'>Number of recoveries from Covid-19</Typography>
+                    </CardContent>
+                </Grid>
+
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color='textSecondary' gutterBottom>Deaths</Typography>
+                        <Typography variant='h5'>REAL DATA</Typography>
+                        <Typography color='textSecondary'>REAL DATE</Typography>
+                        <Typography variant='body2'>Number of Deaths of Covid-19</Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
+        </div>
     )
 }
 
